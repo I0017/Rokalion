cbaae87088df6cbef9a12dd2b0e17d737b6c829d
orrin44 <124921665+orrin44@users.noreply.github.com>
2023-07-12 19:00:18 +0300

    Scene transition isn`t working properly :(

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class M : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     private void Start()
     {
